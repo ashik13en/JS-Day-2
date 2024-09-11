33083be6d11d0c7a2b0631bde05870dfeaa8daf9
ashik13en <ashikurrahman13en@gmail.com>
2024-09-12 04:49:34 +0600

    add js practise code

---
+++ app.js
@@ -0,0 +1,160 @@
+// data types
+
+// 2 type of data in js
+    // 1. primitive
+        // 1. string 
+        // 2. number
+        // 3. boolean
+        // 4. null
+        // 5. undefined
+    // 2. non primitive
+        // 1. object {}
+        // 2. array []
+
+    // object data type   
+
+    // let userInfo = {
+    //     name:"Fuad",
+    //     age:12
+    // }
+
+    // let userInfo = {
+    //     userOne :{
+    //         name:"Fuad",
+    //         age:12
+    //     },
+    //     userTwo :{
+    //         name:"joy",
+    //         age:12
+    //     },
+    // }
+
+    // Array []
+
+    // let userInfo = [15,222,2,3,4,5,68]
+
+    // let userInfo = [
+    //     {
+    //         name:"Fuad",
+    //         age:12
+    //     }
+    // ]
+
+    // let colors = ["red", "green", "blue"];
+
+
+    // console.log(colors)
+
+    // operators 
+
+    // Operators in JavaScript are special symbols or keywords used to perform operations on operands
+
+    // let sum = 5 + 3; // 8
+    // let diff = 10 - 6; // 4
+    // let product = 4 * 2; // 8
+    // let vag = 20 / 5; // 4
+
+    // Increment ++ (Increases value by 1)
+    // let counter = 5;
+    // counter++; // 6
+
+    // -- : Decrement (Decreases value by 1)
+    // let counter = 5;
+    // counter--; // 4
+
+    // 3. Comparison Operators
+    // Comparison operators compare two values and return a boolean (true or false).
+
+    // == : Equal To (checks value, not type)
+    // 5 == "5"; // true
+
+    // === : Strict Equal (checks value and type)
+    // 5 === "5"; // false
+
+    // != : Not Equal To (checks value)
+    // 5 != 3; // true
+
+    // !== : Strict Not Equal (checks value and type)
+    // 5 !== "5"; // true
+
+    // > : Greater Than
+    // 7 > 5; // true
+
+    // < : Less Than
+    // 3 < 4; // true
+
+    // >= : Greater Than or Equal To
+    // 5 >= 5; // true
+
+    // <= : Less Than or Equal To
+    // 3 <= 5; // true
+
+    // 4. Logical Operators
+
+    // && : Logical AND (Returns true if both conditions are true)
+    //let result = (5 > 3) && (2 < 4); // true
+
+
+    // || : Logical OR (Returns true if one of the conditions is true)
+    // let result = (5 < 3) || (2 < 4); // true
+
+    // ! : Logical NOT (Inverts the result)
+    // let result = !(5 > 3); // false
+
+    // condition
+
+    // let age = 17;
+
+    // if(age >= 18){
+    //     console.log("you are a voter")
+    // } else {
+    //     console.log("you are not a voter")
+    // }
+
+    // Conditional (Ternary) Operator
+    // let age = 17;
+
+    // let vote = (age >= 18) ? "You can vote" : "You cannot vote";
+
+    // console.log(vote)
+
+
+    // Concatenation: Combines two strings +
+    // let greeting = "Hello"+ " "  + "World"; // "Hello World"
+    // console.log(greeting)
+
+
+    let username = null;
+    let displayName = username ?? "Guest"; // "Guest"
+    console.log(displayName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
